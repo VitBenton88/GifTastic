9018ea4172ab3b21f53af6c53613f01bb8010b5f
Vit Benton <vitbenton@Macintosh.local>
2017-07-10 00:03:11 -0400

    gif still load done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,48 +1,46 @@
-$(document).ready(function() {
+var keywords = ['Dumb & Dumber','This is Spinal Tap','Old School','Ace Ventura: Pet Detective','The 40-Year-Old Virgin','Superbad','The Hangover','Harold & Kumar Go to White Castle','The LEGO Movie','Shaun of the Dead','Ghostbusters','Office Space',"Wayne's World",'The Big Lebowski','Zoolander'];
 
-	var keywords = ['Dumb & Dumber','This is Spinal Tap','Old School','Ace Ventura: Pet Detective','The 40-Year-Old Virgin','Superbad','The Hangover','Harold & Kumar Go to White Castle','The LEGO Movie','Shaun of the Dead','Ghostbusters','Office Space',"Wayne's World",'The Big Lebowski','Zoolander'];
-	var staticGif = false;
-	var animatedGif = false;
-	var buttonKeyword;
-	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonKeyword + "&api_key=dc6zaTOxFJmzC&limit=10";
+//--------END OF GLOBAL VARIABLES	
 
-	//--------END OF GLOBAL VARIABLES	
-
-	function createButtons (){
-		for (i=0; i<keywords.length; i++){
-			$('#buttonsList').append('<button type="button" class="btn btn-primary">' + keywords[i] + '</button>')
-		};
+function createButtons (){
+	for (i=0; i<keywords.length; i++){
+		$('#buttonsList').append('<button type="button" class="btn btn-primary">' + keywords[i] + '</button>')
 	};
-
+};
+	
 	//--------END OF GLOBAL FUNCTIONS
 
+$(document).ready(function() {
 
 	//push buttons from a keywords array immediately at load
-
 	createButtons();
-
-	console.log(queryURL);
 
 	//When the user clicks on a button, the page should grab 10 static, non-animated gif images...
 		//...from the GIPHY API and place them on the page.
 
 	$('button').on("click",function(){
-		$('#gifList').empty();
-		var buttonKeyword = 'Zoolander';
+		$('.row').empty();
+		var buttonKeyword = $(this).text();
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonKeyword + "&api_key=dc6zaTOxFJmzC&limit=10";
 		$.ajax({
 	      	url: queryURL,
 	      	method: "GET"
 	    	}).done(function(response) {
-	      		$('#gifList').append("<img src=" + response.url + ">");
-	    });
+	    		console.log(buttonKeyword);
+	    		for (i=0; i<10; i++){
+	      			$('.row').append("<div class='col-md-3'><img src=" + response.data[i].images.fixed_height_still.url + "></div>");
+	      		};
+	    	});
 	});
 
 	//When the user clicks one of the still GIPHY images, the gif should animate... 
 		//...If the user clicks the gif again, it should stop playing.
 
+	$('img').on("click",function(){
+
+	});
+
 	//Under every gif, display its rating (PG, G, so on).
-
-
 
 
 	//----------------------------------------------------------------END OF SCRIPT	
